8ebfac44babaeb73c6dc245515b8c1b003af06cc
HeoMiRim <138641470+HeoMiRim@users.noreply.github.com>
2023-07-06 09:39:36 +0900

    server_start.sh

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#sudo chmod +x /home/ec2-user/tomcat/bin/./startup.sh
-#sudo /home/ec2-user/tomcat/bin/./startup.sh
+#sudo chmod +x /root/ec2-user/tomcat/bin/./startup.sh
+#sudo /root/ec2-user/tomcat/bin/./startup.sh
 echo 1
